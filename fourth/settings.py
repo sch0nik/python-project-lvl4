b8ff3956cfb86912a6e03f850502dbac0f644218
Николай Ануфриев <sch-nikolaj@yandex.ru>
2022-02-08 13:42:43 +0300

    Подключил posgresql

--- fourth/settings.py
+++ fourth/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
+import dj_database_url
 
 load_dotenv(find_dotenv())
 
@@ -80,13 +81,7 @@ WSGI_APPLICATION = 'fourth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
