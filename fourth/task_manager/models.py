67f7df6c1a9d12da4f1847f64ac19f82c82577b4
Николай Ануфриев <sch-nikolaj@yandex.ru>
2022-02-22 12:29:04 +0300

    Добавил модель User

--- fourth/task_manager/models.py
+++ fourth/task_manager/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-# Create your models here.
-# model user
-# name
-# last_name
-# username
-# password.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.user_name}'
